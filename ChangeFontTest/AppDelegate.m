738ce2a2621481045c1532b3dfcc768f46965674
Andy Lee <aglee@mac.com>
2019-07-28 02:45:29 -0400

    Change NSFontManager's action to a custom selector.
    Seems to do what I expected.

--- ChangeFontTest/AppDelegate.m
+++ ChangeFontTest/AppDelegate.m
@@ -22,6 +22,20 @@
 	NSLog(@"changeFont: called in %@", self.className);
 }
 
+/**
+ * On launch we tweak NSFontManager to send this message whenever the user makes a change
+ * in the font panel.
+ */
+- (IBAction)appSpecificChangeFont:(id)sender {
+	NSLog(@"appSpecificChangeFont: called in %@", self.className);
+
+	// Note: the BOOL return value is discarded.  Seems like a possible minor flaw in this
+	// approach, as -[NSFontManager sendAction:] will always return YES, which may not
+	// technically be the answer we want if nothing on the responder chain responds to
+	// `changeFont:`.  But in practice maybe we don't care.
+	(void)[NSApp sendAction:@selector(changeFont:) to:nil from:sender];
+}
+
 - (IBAction)makeSenderTheFirstResponder:(id)sender {
 	[self.window makeFirstResponder:sender];
 }
@@ -34,23 +48,31 @@
  * FIXME: Doesn't print the full responder chain.
  *
  * Starts with current FR and follows `nextResponder` until it hits nil.  This is good
- * enough for my quick and dirty purposes, but the full responder chain includes window
- * delegate, app delegate, NSApp, and whatnot.  I'm too lazy to look at the event handling
- * docs right now.
+ * enough for my quick and dirty purposes, but IIRC the full responder chain includes the
+ * app delegate, the NSApp object, and possibly other stuff.  I'm too lazy to look at the
+ * event handling docs right now.
  */
 - (IBAction)printResponderChain:(id)sender {
 	NSResponder *resp = NSApp.keyWindow.firstResponder;
-	NSLog(@"---- BEGIN responder chain");
+	NSLog(@"---- BEGIN incomplete responder chain");
 	do {
 		NSLog(@"%@ -- implements changeFont:? %d", resp.className, [resp respondsToSelector:@selector(changeFont:)]);
 		resp = resp.nextResponder;
 	} while (resp);
-	NSLog(@"----END responder chain");
+	NSLog(@"---- END");
 }
 
 #pragma mark - <NSApplicationDelegate> methods
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
+
+	// Normally when a font is selected in the font panel, NSFontManager sends a
+	// `changeFont:` message to the first responder.  Here we tweak NSFontManager to
+	// instead send an `appSpecificChangeFont:` message.  Presumably this app delegate
+	// will be the only object in the responder chain that responds to the message.
+	NSFontManager.sharedFontManager.action = @selector(appSpecificChangeFont:);
+
+	// Display the font panel as a convenience.
 	[NSFontManager.sharedFontManager orderFrontFontPanel:nil];
 }
 
